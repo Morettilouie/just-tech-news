f6208d8e236cad1ac8d8110fa88d32fbfa9bc297
Louie Moretti <morettilouie@gmail.com>
2022-08-03 23:16:06 -0500

    added hooks to hash passwords

--- models/User.js
+++ models/User.js
@@ -3,7 +3,7 @@ const sequelize = require('../config/connection');
 const bcrypt = require('bcrypt');
 
 // create our User model
-class User extends Model {}
+class User extends Model { }
 
 // define table columns and configuration
 User.init(
@@ -52,9 +52,15 @@ User.init(
                 newUserData.password = await bcrypt.hash(newUserData.password, 10);
                 return newUserData;
             },
-        }
-    },
-    {
+            // set up beforeUpdate lifecycle "hook" functionality
+            async beforeUpdate(updatedUserData) {
+                updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+                return updatedUserData;
+            },
+        },
+
+
+
         // TABLE CONFIGURATION OPTIONS GO HERE (https://sequelize.org/v5/manual/models-definition.html#configuration))
 
         // pass in our imported sequelize connection (the direct connection to our database)
