f04f26e48a336e742580973ddbf5e4c3293bfe29
Louie Moretti <morettilouie@gmail.com>
2022-08-04 00:29:13 -0500

    login route and verification

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -50,6 +50,29 @@ router.post('/', (req, res) => {
         });
 });
 
+// Login route
+router.post('/login', (req, res) => {
+    // expects {username: 'test', email: 'test', email: 'test', password: 'test'}
+    User.findOne({
+        where: {
+            email: req.body.email
+        }
+    }).then(dbUserData => {
+        if (!dbUserData) {
+            res.status(400).json({ message: 'No user with that email address!' });
+            return;
+        }
+        // Verify user
+        const validPassword = dbUserData.checkPassword(req.body.password);
+        if (!validPassword) {
+            res.status(400).json({ message: 'Incorrect password!' });
+            return;
+        }
+
+        res.json({ user: dbUserData, message: 'You are now logged in!' });
+    })
+})
+
 // PUT /api/users/1
 router.put('/:id', (req, res) => {
     // if req.body has exact key/value pairs to match the model, you can just use `req.body` instead
