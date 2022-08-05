796e24d2e864809823db63aa9db0912e2ff878fe
Louie Moretti <morettilouie@gmail.com>
2022-08-05 17:09:08 -0500

    added upvotes and able to view in get requests of users

--- models/User.js
+++ models/User.js
@@ -2,7 +2,7 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 const bcrypt = require('bcrypt');
 
-// create our User model
+// create User model
 class User extends Model {
     // set up to run on instance data (per user) to check password
     checkPassword(loginPw) {
